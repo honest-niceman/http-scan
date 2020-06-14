b29a1d473c77ae3fa0e765d7ffaada5a47f91501
honest-niceman <56090398+honest-niceman@users.noreply.github.com>
2020-06-14 18:25:38 +0400

    Add files via upload

---
+++ Laba2_HTTP/Laba2_HTTP/Form1.Designer.cs
@@ -0,0 +1,200 @@
+﻿namespace Laba2_HTTP
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.textBox_URI = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.TextBox_Deep = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.AliceBlue;
+            this.button1.Location = new System.Drawing.Point(313, 178);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(186, 51);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Поехали";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.HorizontalScrollbar = true;
+            this.listBox1.ItemHeight = 20;
+            this.listBox1.Location = new System.Drawing.Point(12, 269);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(371, 524);
+            this.listBox1.TabIndex = 1;
+            // 
+            // textBox_URI
+            // 
+            this.textBox_URI.Location = new System.Drawing.Point(423, 25);
+            this.textBox_URI.Name = "textBox_URI";
+            this.textBox_URI.Size = new System.Drawing.Size(186, 26);
+            this.textBox_URI.TabIndex = 4;
+            this.textBox_URI.Text = "http://hpc.ssau.ru";
+            this.textBox_URI.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(492, 108);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 20);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(492, 144);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(51, 20);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "label2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(205, 70);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(127, 20);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Глубина поиска";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(205, 28);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(61, 20);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Адрес ";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(52, 236);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(682, 20);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Будут найдены ссылки и суммарный размер следующих типов файлов: .doc .txt .pdf .d" +
+    "ocx";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(205, 108);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(256, 20);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "Количество найденных файлов:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(205, 144);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(221, 20);
+            this.label7.TabIndex = 12;
+            this.label7.Text = "Размер найденных файлов:";
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.HorizontalScrollbar = true;
+            this.listBox2.ItemHeight = 20;
+            this.listBox2.Location = new System.Drawing.Point(413, 269);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(371, 524);
+            this.listBox2.TabIndex = 13;
+            // 
+            // TextBox_Deep
+            // 
+            this.TextBox_Deep.Location = new System.Drawing.Point(423, 67);
+            this.TextBox_Deep.Name = "TextBox_Deep";
+            this.TextBox_Deep.Size = new System.Drawing.Size(186, 26);
+            this.TextBox_Deep.TabIndex = 14;
+            this.TextBox_Deep.Text = "2";
+            this.TextBox_Deep.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(800, 807);
+            this.Controls.Add(this.TextBox_Deep);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox_URI);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Laba2";
+            this.Load += new System.EventHandler(this.Form1_Load_1);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.TextBox textBox_URI;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.TextBox TextBox_Deep;
+    }
+}
+
---
+++ Laba2_HTTP/Laba2_HTTP/Form1.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Forms;
+using HtmlAgilityPack;
+using System.Net.Sockets;
+using HtmlDocument = HtmlAgilityPack.HtmlDocument;
+using System.Linq;
+namespace Laba2_HTTP
+{
+    public partial class Form1 : Form
+    {
+        public Form1() { 
+            InitializeComponent();
+            label1.Visible = false;
+            label2.Visible = false;
+            label6.Visible = false;
+            label7.Visible = false;
+        }
+        private void Form1_Load(object sender, EventArgs e) { }
+        private string[] exp = { ".doc",".txt",".docx",".pdf"};
+        private static Socket GetSocket(string host, int port)
+        {
+            Socket s = null;
+            var tempSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            try
+            {
+                tempSocket.Connect(host, port);
+            }
+            catch (SocketException ex) { return null; }
+            if (tempSocket.Connected) s = tempSocket;
+            return s;
+        }
+        private static string GetPage(string host, int port, string pages)
+        {
+                string request = pages != "" ? "GET " + pages + " HTTP/1.1\r\n" + "Host: " + host + "\r\nConnection: Close\r\n\r\n" : "GET / HTTP/1.1\r\nHost: " + host + "\r\nConnection: Close\r\n\r\n";
+                Byte[] bytesSent = Encoding.Default.GetBytes(request);
+                Byte[] bytesReceived = new Byte[256];
+                Socket s = GetSocket(host, port);
+                if (s == null) return "Connection failed";
+                s.Send(bytesSent, bytesSent.Length, 0);
+                int bytes;
+                string page = "";
+                do
+                {
+                    bytes = s.Receive(bytesReceived, 0, bytesReceived.Length, 0);
+                    page = page + Encoding.Default.GetString(bytesReceived, 0, bytes);
+                }
+                while (bytes > 0);
+                return page;
+        }
+        private static List<Uri> FindLinks(string responseData, Uri linkForResponse)
+        {
+            HtmlDocument doc = new HtmlDocument();
+            doc.LoadHtml(responseData);
+            List<Uri> urlList = new List<Uri>();
+            if (doc.DocumentNode?.SelectNodes(@"//a[@href]") == null) return urlList;
+            foreach (HtmlNode link in doc.DocumentNode.SelectNodes(@"//a[@href]"))
+            {
+                try
+                {
+                    HtmlAttribute attribute = link.Attributes["href"];
+                    if (attribute == null) continue;
+                    string href = attribute.Value;
+                    if (href.StartsWith("javascript", StringComparison.InvariantCultureIgnoreCase)) continue;
+                    Uri urlNext = new Uri(href, UriKind.RelativeOrAbsolute);
+                    urlNext = new Uri(linkForResponse, urlNext);
+                    if (!urlList.Contains(urlNext))
+                    {
+                        urlList.Add(urlNext);
+                    }
+                }
+                catch (Exception e){}
+            }
+            return urlList;
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            listBox2.Items.Clear();            
+            List<String> allLinks1 = new List<string>();
+            List<String> rightLinks1 = new List<string>();
+            List<Uri> allLinks2 = new List<Uri>();
+            List<Uri> rightLinks2 = new List<Uri>();
+            int size = 0;
+            int Deep = int.Parse(TextBox_Deep.Text);
+            int k = 0;
+            int l = -1;
+            List<Uri> urlsList = new List<Uri>();
+            bool result = Uri.TryCreate(textBox_URI.Text, UriKind.Absolute, out var uriResult) && uriResult.Scheme == Uri.UriSchemeHttp;
+            urlsList.Add(uriResult);
+            string buf = "";
+            for (int i = 0; i < Deep; i++)
+            {
+                List<Uri> allLinks = new List<Uri>();
+                List<Uri> rightLinks = new List<Uri>();
+                k = 0;
+                foreach (var uri in urlsList)
+                {
+                    string page = GetPage(uri.Host, 80, uri.LocalPath);
+                    List<Uri> LinksOnPage = FindLinks(page, uri);
+                    foreach (var findedUri in LinksOnPage)
+                    {
+                        if (findedUri.Host == uriResult.Host && !allLinks.Contains(findedUri))
+                        {
+                            allLinks.Add(findedUri);
+                            allLinks2.Add(findedUri);
+                            string str = findedUri.ToString();
+                            int j = str.Length - 1;
+                            while (!str[j].Equals('.'))
+                            {
+                                j--;
+                            }
+                            buf = str.Substring(j, str.Length - j);
+                            for (int q = 0; q < exp.Length; q++)
+                            {
+                                if (buf.Equals(exp[q]))
+                                {
+                                    rightLinks2.Add(findedUri);
+                                    rightLinks.Add(findedUri);
+                                    size += page.Length;
+                                    k++;
+                                }
+                            }
+                        }
+                    }
+                }
+                urlsList = allLinks;
+            }
+            for (int j = 0; j < allLinks2.Count; j++)
+            {
+                allLinks1.Add(allLinks2[j].ToString());
+            }
+            for (int j = 0; j < rightLinks2.Count; j++)
+            {
+                rightLinks1.Add(rightLinks2[j].ToString());
+            }
+            allLinks1 = allLinks1.Distinct().ToList();
+            rightLinks1 = rightLinks1.Distinct().ToList();
+            for (int j = 0; j < allLinks1.Count; j++)
+            {
+                listBox1.Items.Add((j + 1) + ") " + allLinks1[j]);
+            }
+            for (int j = 0; j < rightLinks1.Count; j++)
+            {
+                listBox2.Items.Add((j + 1) + ") " + rightLinks1[j]);
+            }
+            label1.Visible = true;
+            label2.Visible = true;
+            label6.Visible = true;
+            label7.Visible = true;
+            label1.Text = "" + k + " Штук";
+            label2.Text = "" + size + " Байт";
+        }
+        private void Form1_Load_1(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
